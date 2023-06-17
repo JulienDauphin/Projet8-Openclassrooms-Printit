0104b7d122edd5c4e36bd505ffb7d15fd5980320
JulienDauphin <juliendauphin.openclassrooms@gmail.com>
2023-06-17 09:05:40 +0200

    code modifié / commentaires ajouté

--- assets/script.js
+++ assets/script.js
@@ -1,50 +1,53 @@
-const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+// Encapsulation du code dans une fonction auto-exécutée pour éviter les collisions de variables
+(function() {
+  const slides = [
+    {
+      image: "slide1.jpg",
+      tagLine: "Impressions tous formats <span>en boutique et en ligne</span>"
+    },
+    {
+      image: "slide2.jpg",
+      tagLine: "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+    },
+    {
+      image: "slide3.jpg",
+      tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>"
+    },
+    {
+      image: "slide4.png",
+      tagLine: "Autocollants <span>avec découpe laser sur mesure</span>"
+    }
+  ];
 
 
-const arrowRight = document.querySelector(".arrow_right");
-const arrowLeft = document.querySelector(".arrow_left");
-const dotsContainer = document.querySelector(".dots");
-let dots = [];
-let position = 0;
+  const bannerImg = document.querySelector(".banner-img");
+  const bannerText = document.querySelector("#banner p");
+  const arrowRight = document.querySelector(".arrow_right");
+  const arrowLeft = document.querySelector(".arrow_left");
+  const dotsContainer = document.querySelector(".dots");
+  let position = 0;
+  let dots = [];
+  
 
+  // Fonction pour changer l'image et le texte
+  function changeSlide() {
+    bannerImg.src = "./assets/images/slideshow/" + slides[position].image;
+    bannerText.innerHTML = slides[position].tagLine;
+  }
 
+  // Fonction pour vérifier et ajuster la position
+  function checkPosition() {
+	// Si la position devient supérieure au nombre d'élément de slides alors position = 0 pour boucler le diaporama
+    if (position >= slides.length) {
+      position = 0;
+	//   Et si la position devient négative alors position = au nombre d'élément de slides - 1 pour boucler le diaporama
+    } else if (position < 0) {
+      position = slides.length - 1;
+    }
+  }
 
-  // Clic sur la flèche de droite 
-  arrowRight.addEventListener("click", function() {
-    console.log("Clic sur la flèche de droite");
-  });
-
-  // Clic sur la flèche de gauche 
-  arrowLeft.addEventListener("click", function() {
-    console.log("Clic sur la flèche de gauche");
-  });
-
-  // Création des points
-  slides.forEach(() => {
-    const dot = document.createElement("div");
-    dot.classList.add("dot");
-    dotsContainer.appendChild(dot);
-    dots.push(dot);
-  });
-
-  // Affichage point en cours
+  // Fonction pour mettre à jour les points avec 2 paramètres dans la méthode forEach
+  //  (dot = point en cours d'itération et index = index de l'élément dans le tableau (auto))
   function updateDots() {
     dots.forEach((dot, index) => {
       dot.classList.remove("dot_selected");
@@ -54,34 +57,46 @@ let position = 0;
     });
   }
 
-  updateDots();
-
-  const bannerImg = document.querySelector(".banner-img");
-  const bannerText = document.querySelector("#banner p");
-
-// Changement Slide  
-  function changeSlide() {
-    bannerImg.src = "./assets/images/slideshow/" + slides[position].image;
-    bannerText.innerHTML = slides[position].tagLine;
-  }
-
-// Clic sur une flèche
+  // Fonction lors du clic sur une flèche (droite ou gauche) position = direction +1 ou -1 et on lance nos fonctions de vérif et changement
   function ArrowClic(direction) {
     position += direction;
+    checkPosition();
     updateDots();
     changeSlide();
   }
 
-    // Clic sur la flèche de droite 
-    arrowRight.addEventListener("click", function() {
-        console.log("Clic sur la flèche de droite");
-        console.log(position);
-        ArrowClic(1);
-      });
-    
-      // Clic sur la flèche de gauche 
-      arrowLeft.addEventListener("click", function() {
-        console.log("Clic sur la flèche de gauche");
-        console.log(position);
-        ArrowClic(-1);
-      });
+  // Création des points (1 Div avec classe dot pour chaque élément du tableau slides)
+  slides.forEach(() => {
+    const dot = document.createElement("div");
+    dot.classList.add("dot");
+    dotsContainer.appendChild(dot);
+    dots.push(dot);
+  });
+
+  // Clic sur la flèche de droite donc je définis direction à 1 dans la fonction ArrowClic
+  arrowRight.addEventListener("click", function() {
+    ArrowClic(1);
+    console.log("Clic sur le bouton de droite");
+    console.log(position);
+  });
+
+  // Clic sur la flèche de gauche donc je définis direction à -1 dans la fonction ArrowClic
+  arrowLeft.addEventListener("click", function() {
+    ArrowClic(-1);
+    console.log("Clic sur le bouton de gauche");
+    console.log(position);
+  });
+
+  // Clic sur un point
+  dots.forEach((dot, index) => {
+    dot.addEventListener("click", function() {
+      position = index;
+      updateDots();
+      changeSlide();
+    });
+  });
+
+  // Affichage initial du premier slide pour sécuriser l'affichage de départ sur 0 (position étant égal à 0)
+  changeSlide();
+  updateDots();
+})();
